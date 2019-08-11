4b952b6b4da370ed893135ed0b3fd128eb99257a
27917297 <nada.zelic@gmail.com>
2019-08-11 09:53:16 +0200

    Style buttons

--- calculator/src/components/Keys/Operators/Operators.js
+++ calculator/src/components/Keys/Operators/Operators.js
@@ -3,7 +3,7 @@ import classes from './Operators.module.css';
 
 const operators = props => (
     <div className={classes.Operators}>
-        <button onClick={() => props.clicked('+')}>+</button>
+        <button onClick={() => props.clicked('+')} style={{marginTop: '18px'}}>+</button>
         <button onClick={() => props.clicked('-')}>-</button>
         <button onClick={() => props.clicked('*')}>x</button>
         <button onClick={() => props.clicked('/')}>/</button>
